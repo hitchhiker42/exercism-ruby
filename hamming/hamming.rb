82b6030a5cd01538e8338abe64a97f32539c08e5
Isha <aysha.edathodu@gmail.com>
2015-10-11 14:50:54 -0400

    refactored

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,27 +1,23 @@
 class Hamming
 
 	VERSION = 1
-	
+
 	def self.compute(strandA, strandB)
 		a = strandA.split("")
 		b = strandB.split("")
 		distance = 0
 		i = 0
-		if a.length != b.length
-			raise ArgumentError
-		else
-			while i <= a.length
-				if a[i] != b[i]
-					distance += 1
-					# print a[i]
-					i += 1
-				else 
-					i += 1
-				end
-			end
-			distance 
-		end
-	end
 
-	# compute('GGACGGATTCTG', 'AGGACGGATTCT')
+		while i <= a.length
+			if a.length != b.length
+				raise ArgumentError
+			elsif a[i] != b[i]
+				distance += 1
+				i += 1
+			else
+				i += 1
+			end
+		end
+		distance
+	end
 end 
