09455c81f4636904d5c4dc662cce58c272a95404
Isha <aysha.edathodu@gmail.com>
2015-10-13 18:03:13 -0400

    refactored

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,60 +1,45 @@
 class Complement
 
 	VERSION = 2
+
+	def self.join_together(input_str, output_str)
+		if input_str.length == output_str.length
+			input_str.join('')
+		else
+			raise ArgumentError
+		end
+	end
 	
 	def self.of_dna(dnastrand)
 		dna = dnastrand.split('')
 		dna_complements = {"A" => "U", "G" => "C", "C" => "G", "T" => "A"}
 		rna = []
 
-		# dna.each do |n|
-		# 	if n != ('A' || 'G' || 'T' || 'C')
-		# 		raise ArgumentError
-		# 	else
-		# 		dna_complements.each do |k,v|
-		# 			if n == k
-		# 				rna << v 
-		# 			end
-		# 		end
-		# 	rna.join('')
-		# 	end
-		# end
-
-		i = 0
-		while i <= dna.length
-			dna_complements.each do |k,v|
-				# if (dna[i] == dna.length) && (dna[i] != k)
-				# 	raise ArgumentError
-				# 	puts dna[i]
-				if dna[i] == k
-					rna << v 
+		dna.each do |n|
+			dna_complements.each do |key,value|
+				if n == key
+					rna << value 
 				end
 			end
-			i += 1
 		end
-
-		if rna.length == dna.length
-			rna.join('')
-		else
-			raise ArgumentError
-		end
+	join_together(rna, dna)	
 	end
 
-	# def self.of_rna(rnastrand)
-	# 	rna = rnastrand.split('')
-	# 	rna_complements = {"U" => "A", "G" => "C", "C" => "G", "A" => "T"}
-	# 	dna = []
+	def self.of_rna(rnastrand)
+		rna = rnastrand.split('')
+		rna_complements = {"U" => "A", "G" => "C", "C" => "G", "A" => "T"}
+		dna = []
 
-	# 	rna.each do |n|
-	# 		rna_complements.each do |k,v|
-	# 			if n == k
-	# 				dna << v
-	# 			end
-	# 		end
-	# 	end
-	# 	dna.join('')
-	# end
+		rna.each do |n|
+			rna_complements.each do |k,v|
+				if n == k
+					dna << v
+				end
+			end
+		end
+		join_together(dna, rna)	
+	end
 
 end
 
-Complement.of_dna('ACGTGGTCTTAA')
+# Complement.of_dna('ACGTGGTCTTAA')
