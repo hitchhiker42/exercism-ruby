0e839b17d0d239dc14b3b822c07d46ae67408bf7
Isha <aysha.edathodu@gmail.com>
2015-10-15 00:27:45 -0400

    in progress to refactor of_rna

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -2,44 +2,32 @@ class Complement
 
 	VERSION = 2
 
-	def self.join_together(input_str, output_str)
-		if input_str.length == output_str.length
-			input_str.join('')
-		else
-			raise ArgumentError
-		end
-	end
-	
+   @@dna_complements = {"A" => "U", "G" => "C", "C" => "G", "T" => "A"}
+   @@rna_complements = @@dna_complements.inverse
+
 	def self.of_dna(dnastrand)
 		dna = dnastrand.split('')
-		dna_complements = {"A" => "U", "G" => "C", "C" => "G", "T" => "A"}
 		rna = []
 
-		dna.each do |n|
-			dna_complements.each do |key,value|
-				if n == key
-					rna << value 
-				end
-			end
+		dna.map do |n|
+			@@dna_complements[n] == nil ? (raise ArgumentError) : (rna << @@dna_complements[n])
 		end
-	join_together(rna, dna)	
-	end
+		rna.join('')
+	end	
 
-	def self.of_rna(rnastrand)
-		rna = rnastrand.split('')
-		rna_complements = {"U" => "A", "G" => "C", "C" => "G", "A" => "T"}
-		dna = []
+   def self.of_rna(rnastrand)
+      rna = rnastrand.split('')
+      dna = []
 
-		rna.each do |n|
-			rna_complements.each do |k,v|
-				if n == k
-					dna << v
-				end
-			end
-		end
-		join_together(dna, rna)	
-	end
-
+      rna.map do |n|
+         # puts @@dna_complements.key(n)
+         # @@dna_complements.key(n) == nil ? (raise ArgumentError) : (dna << @@dna_complements.key(n))
+         @@rna_complements[n] == nil ? (raise ArgumentError) : (dna << @@rna_complements[n])
+      end
+      puts dna.join('')
+   end
+	
 end
 
-# Complement.of_dna('ACGTGGTCTTAA')
+Complement.of_rna('UGAACCCGACAUG')
+Complement.of_rna('T')
