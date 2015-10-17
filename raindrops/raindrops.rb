baa21cb033a2417b47a38daeed7802edd168fbed
Isha <aysha.edathodu@gmail.com>
2015-10-17 12:21:16 -0400

    refactored with map

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -2,26 +2,13 @@ require 'prime'
 
 class Raindrops
 
-	def self.string_conv(result_arr, num)
-		if result_arr == []
-			(result_arr << "#{num}").join('')
-		else
-			result_arr.join('')
-		end
-	end
+  def self.convert(num)
+  conversion = { 3 => "Pling", 5 => "Plang", 7 => "Plong"}
+  all_primes = Prime.prime_division(num).flatten
 
-	def self.convert(num)
-	result = []
-	conversion = { 3 => "Pling", 5 => "Plang", 7 => "Plong"}
-	all_primes = Prime.prime_division(num).flatten
+    result = all_primes.map{ |prime| prime = conversion[prime] }.join('')
 
-		all_primes.each do |prime|
-			conversion.each do |key, value|
-				if prime == key
-					result << value
-				end
-			end
-		end
-		string_conv(result, num)
-	end
+    result == "" ? num.to_s : result
+  end
+
 end
