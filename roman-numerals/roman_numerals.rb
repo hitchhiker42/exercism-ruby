896a24765c83449be369a240eaff1e95fd7aa33d
Isha <aysha.edathodu@gmail.com>
2015-10-21 23:26:59 -0400

    tests all pass, using giant conversion table

---
+++ roman-numerals/roman_numerals.rb
@@ -0,0 +1,54 @@
+class Fixnum
+
+	def to_roman
+
+      conversion_hash = {
+         1 => "I",
+         2 => "II", 
+         3 => "III",
+         4 => "IV",
+         5 => "V",
+         6 => "VI", 
+         7 => "VII", 
+         8 => "VIII",
+         9 => "IX",
+         10 => "X",
+         20 => "XX", 
+         30 => "XXX",
+         40 => "XL",
+         50 => "L",
+         60 => "LX", 
+         70 => "LXX", 
+         80 => "LXXX",
+         90 => "XC",
+         100 => "C",
+         200 => "C", 
+         300 => "C",
+         400 => "CD",
+         500 => "D",
+         600 => "DC", 
+         700 => "DCC", 
+         800 => "DCCC",
+         900 => "CM",   
+         1000 => "M",
+         2000 => "MM",
+         3000 => "MMM"
+      }
+
+      # year_array = self.to_s.chars.map(&:to_i)
+      decimal_places = [1,10,100,1000]
+      decimal_year = (self.to_s.chars.map(&:to_i).reverse).zip decimal_places
+      value_list = []
+      roman = []
+
+      decimal_year.each do |i|
+         value_list << i.inject(:*)
+      end
+      
+      p value_list.map { |val| conversion_hash[val]}.reverse.join('')
+
+	end
+
+end
+
+27.to_roman
