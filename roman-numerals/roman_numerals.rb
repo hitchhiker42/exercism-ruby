95b978c7ac3771f8d5921fcb5aadec559e01dd2e
Isha <aysha.edathodu@gmail.com>
2015-10-24 23:14:11 -0400

    hash with fewer values

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -4,44 +4,44 @@ class Fixnum
 
       conversion_hash = {
          1 => "I",
-         2 => "II", 
-         3 => "III",
          4 => "IV",
          5 => "V",
-         6 => "VI", 
-         7 => "VII", 
-         8 => "VIII",
          9 => "IX",
          10 => "X",
-         20 => "XX", 
-         30 => "XXX",
          40 => "XL",
          50 => "L",
-         60 => "LX", 
-         70 => "LXX", 
-         80 => "LXXX",
          90 => "XC",
          100 => "C",
-         200 => "C", 
-         300 => "C",
          400 => "CD",
          500 => "D",
-         600 => "DC", 
-         700 => "DCC", 
-         800 => "DCCC",
          900 => "CM",   
-         1000 => "M",
-         2000 => "MM",
-         3000 => "MMM"
+         1000 => "M"
       }
 
+      year_array = self.to_s.chars.map(&:to_i).reverse
       decimal_places = [1,10,100,1000]
       decimal_year = (self.to_s.chars.map(&:to_i).reverse).zip decimal_places
 
-      decimal_year.map! { |i| i.inject(:*)}
+      decimals = decimal_year.map { |i| i.inject(:*) }
+      value = []
+      i = 0
+      while i < year_array.length
 
-      decimal_year.map { |val| conversion_hash[val]}.reverse.join('')
+      roman_num = conversion_hash[decimals[i]]
+      prev_roman_num = conversion_hash[decimal_places[i]]
+      
+         if roman_num != nil
+            value << roman_num
+         elsif decimals[i] == 0
+            value << ""
+         elsif year_array[i].to_i < 5
+            value << prev_roman_num.to_s * year_array[i] 
+         elsif year_array[i].to_i > 5
+            value << conversion_hash[decimal_places[i] * 5].to_s + (prev_roman_num * (year_array[i] - 5))
+         end 
 
+         i +=1
+      end
+      value.reverse.join('')    
 	end
-
 end
---
+++ roman-numerals/roman_numerals_backup.rb
@@ -0,0 +1,47 @@
+roman_numerals_backup.rbclass Fixnum
+
+	def to_roman
+
+      conversion_hash = {
+         1 => "I",
+         2 => "II", 
+         3 => "III",
+         4 => "IV",
+         5 => "V",
+         6 => "VI", 
+         7 => "VII", 
+         8 => "VIII",
+         9 => "IX",
+         10 => "X",
+         20 => "XX", 
+         30 => "XXX",
+         40 => "XL",
+         50 => "L",
+         60 => "LX", 
+         70 => "LXX", 
+         80 => "LXXX",
+         90 => "XC",
+         100 => "C",
+         200 => "C", 
+         300 => "C",
+         400 => "CD",
+         500 => "D",
+         600 => "DC", 
+         700 => "DCC", 
+         800 => "DCCC",
+         900 => "CM",   
+         1000 => "M",
+         2000 => "MM",
+         3000 => "MMM"
+      }
+
+      decimal_places = [1,10,100,1000]
+      decimal_year = (self.to_s.chars.map(&:to_i).reverse).zip decimal_places
+
+      decimal_year.map! { |i| i.inject(:*)}
+
+      decimal_year.map { |val| conversion_hash[val]}.reverse.join('')
+
+	end
+
+end
