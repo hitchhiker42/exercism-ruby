055df00862d2437ab348675ab0541158c2c7d145
Isha <aysha.edathodu@gmail.com>
2015-11-12 03:00:22 -0500

    hamming refactored, next step implement with each_char

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -3,11 +3,12 @@ class Hamming
 VERSION = 1
 
   def self.compute(strand_a, strand_b)
-    a = strand_a.each_char { |c| c }
-    b = strand_b.chars
 
-    raise ArgumentError, 'unequal string lengths' unless a.length == b.length
-      (0...a.length).count { |index| a[index] != b[index] }
+    raise ArgumentError, 'unequal string lengths' unless strand_a.length == strand_b.length
+    	(0...strand_a.length).count { |index| strand_a.chars[index] != strand_b.chars[index] }
+
+    # raise ArgumentError, 'unequal string lengths' unless strand_a.length == strand_b.length
+    # 	strand_a.each_char.count { |c| c != strand_b.chars[index] }
 
   end
 
